6fc7694426af4c5319c4819b7b912f4a282c6d61
yaelmtzk <jessianeroque@gmail.com>
2025-10-30 17:13:42 +0200

    Remove header string from strHTMLs

--- js/book.controller.js
+++ js/book.controller.js
@@ -11,14 +11,7 @@ function render() {
     
     const books = getBooks()
 
-    var strHTMLs = 
-            `<tr class="header">
-                <th>Title</th>
-                <th>Price</th>
-                <th>Actions</th>
-            </tr>`
-
-    strHTMLs = books.map(book =>
+    var strHTMLs = books.map(book =>
         `<tr >
             <td class="title">${book.title}</td>
             <td class="price">${book.price}</td>
