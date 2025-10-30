2ab1063ca811ed65a75e94e0e2eb7b452e419857
yaelmtzk <jessianeroque@gmail.com>
2025-10-30 16:49:27 +0200

    Set render() to generate a html string with books obj list and render it

--- js/book.controller.js
+++ js/book.controller.js
@@ -9,4 +9,26 @@ function onInit() {
 
 function render() {
     
+    const books = getBooks()
+
+    var strHTMLs = 
+            `<tr class="header">
+                <th>Title</th>
+                <th>Price</th>
+                <th>Actions</th>
+            </tr>`
+
+    strHTMLs = books.map(book =>
+        `<tr >
+            <td class="title">${book.title}</td>
+            <td class="price">${book.price}</td>
+            <td class="action-btns">
+                <button class="btn-read">Read</button>
+                <button class="btn-update">Update</button>
+                <button class="btn-delete">Delete</button>
+            </td>
+        </tr>`
+    )
+
+    document.querySelector('.books').innerHTML = strHTMLs.join('')
 }
