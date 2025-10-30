92d6625e14cb46eec1f7e2f30f815e62e365fe5c
yaelmtzk <jessianeroque@gmail.com>
2025-10-30 16:24:36 +0200

    Create book service js  & getBooks() that returns gBooks - hardcoded books objects list

---
+++ js/book.service.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const gBooks = [
+  {
+    id: 1,
+    title: 'The Art of War',
+    price: 15,
+    imgUrl: 'https://images.meesho.com/images/products/548939233/4nnvn_512.avif?width=512'
+  },
+  {
+    id: 2,
+    title: 'A Brief History of Time',
+    price: 30,
+    imgUrl:'https://bf1af2.akinoncloudcdn.com/products/2024/12/06/220452/3dbfe013-d5d7-4423-9a46-699fd31750ec_size256_cropCenter.jpg'
+  },
+  {
+    id: 3,
+    title: 'Clean Code',
+    price: 40,
+    imgUrl: 'https://mateuscosta.me/assets/images/books/clean_code.png'
+  },
+  {
+    id: 4,
+    title: 'Pride and Prejudice',
+    price: 25,
+    imgUrl: 'https://files.idyllic.app/files/static/2143329?width=256&optimizer=image'
+  },
+  {
+    id: 5,
+    title: 'The Selfish Gene',
+    price: 35,
+    imgUrl: 'https://play-lh.googleusercontent.com/-0R2fsJQfH-KGve9cRaTYy8EgOsHoECT6Tu5eJ0ML5eOrXEguc81McwWtAcqYuBse3qVdXQKHo_bnik=s256-rw'
+  }
+]
+
+function getBooks(){
+
+    return gBooks()
+
+}
